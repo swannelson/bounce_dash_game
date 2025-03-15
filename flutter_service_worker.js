edcd26b7b6218d6a1119ff3034805b38b217863e
swannelson <133827181+swannelson@users.noreply.github.com>
2025-03-15 17:34:39 -0400

    Fix base href for GitHub Pages

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,8 +23,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "a3d922a0b0bb96200631d1d9f758ce7d
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "adc8a51586f386fd3661f23bbf72c9d1",
-"/": "adc8a51586f386fd3661f23bbf72c9d1",
+"index.html": "8dbb5da1268d9d8068530ce08cdd07ff",
+"/": "8dbb5da1268d9d8068530ce08cdd07ff",
 "main.dart.js": "9eca0d5a86ae9d082e058b97b246c7cc",
 "manifest.json": "f8de551dd40ceda975a7924ce3931f46",
 "version.json": "3202dafbb3a2b5d222fce22dd5d82cc1"};
